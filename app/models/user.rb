a6c188443b233c8c1c9c2d44ca348a6f57011043
Jake Smith <jakehockey10@gmail.com>
2015-05-21 21:40:55 -0600

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }
+  # has_secure_password enforces presence validations upon object creation.
+  validates :password, length: { minimum: 6 }, allow_blank: true
   has_secure_password
 
   class << self
